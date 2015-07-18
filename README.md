0eae2ee8a15df47479e5f9e006bd2a3c8abd8960
Roberto Ortega <berto.ort@gmail.com>
2015-07-18 11:51:31 -0600

    steve it

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # steveit
 
+https://obscure-ridge-5842.herokuapp.com/
+
 Confused about the assignment? have a deep, convoluted question and feel too lazy to google it? steve it!
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   unirest.post('https://hooks.slack.com/services/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')
-  .send('payload={"text": "'+ req.body.question +'", "channel": "@dzincone", "username": "steve it", "icon_emoji": ":trollface:"}')
+  .send('payload={"text": "'+ req.body.question +'", "channel": "@sabrams86", "username": "steve it", "icon_emoji": ":trollface:"}')
   .end(function (response) {
   res.redirect('/');
   });
