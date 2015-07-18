bd0d4a591f7c399478d07a1d36f5514309c0842f
Roberto Ortega <berto.ort@gmail.com>
2015-07-18 11:49:53 -0600

    it works

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # steveit
+
+Confused about the assignment? have a deep, convoluted question and feel too lazy to google it? steve it!
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+  unirest.post('https://hooks.slack.com/services/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')
+  .send('payload={"text": "'+ req.body.question +'", "channel": "@dzincone", "username": "steve it", "icon_emoji": ":trollface:"}')
+  .end(function (response) {
+  res.redirect('/');
+  });
 });
 
 module.exports = router;
